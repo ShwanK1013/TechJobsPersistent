518e8f8aec406d3e01f47195033d33d07c4735e4
saehwan kim <saehwan1013@gmail.com>
2021-08-21 11:07:26 -0700

    sql query added

--- TechJobsPersistent/queries.sql
+++ TechJobsPersistent/queries.sql
@@ -1,6 +1,6 @@
 --Part 1
-
+ select Id, Name, EmployerId from jobs
 --Part 2
-
+select Name from emloyers where (Location = "St. Louis City")
 --Part 3
-
+select Name,Description from skills INNER JOIN jobskills on skills.Id = jobskills.SkillId order by Name 
